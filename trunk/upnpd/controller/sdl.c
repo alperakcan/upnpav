f409bb343514fc0345b59cfa8e51b96708c9b066
anhanguera <anhanguera@d8938883-667f-4658-bccc-02aa132b77ea>
2009-05-11 12:32:20 +0000

    ...

---
+++ trunk/upnpd/controller/sdl.c
@@ -0,0 +1,28 @@
+/***************************************************************************
+    begin                : Sun Jun 01 2008
+    copyright            : (C) 2008 by Alper Akcan
+    email                : alper.akcan@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation; either version 2.1 of the  *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ ***************************************************************************/
+
+#include <config.h>
+
+#include "controller.h"
+
+static int sdl_main (char *options)
+{
+	return 0;
+}
+
+controller_gui_t controller_sdl = {
+	.name = "sdl",
+	.main = sdl_main,
+};
