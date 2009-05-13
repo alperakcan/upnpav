3a6d644da4ff64c7174326e1a5eaa3cff7f86b2d
anhanguera <anhanguera@d8938883-667f-4658-bccc-02aa132b77ea>
2009-05-13 00:56:35 +0000

    metadata, basic ui

--- trunk/metadata/metadata.c
+++ trunk/metadata/metadata.c
@@ -1,3 +1,17 @@
+/***************************************************************************
+    begin                : Sun Jun 01 2008
+    copyright            : (C) 2008 - 2009 by Alper Akcan
+    email                : alper.akcan@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation; either version 2.1 of the  *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ ***************************************************************************/
 
 #include <config.h>
 
